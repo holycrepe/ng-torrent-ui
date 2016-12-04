f5550214663e96ac393a7e7463235bdc225906f1
Avinash Puchalapalli <git@puchalapalli.com>
2016-12-04 07:17:30 -0500

    Update live reload host to my personal setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,6 +165,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
+        hostname: '192.168.0.10',
         livereload: 35729
       },
       proxies: [
